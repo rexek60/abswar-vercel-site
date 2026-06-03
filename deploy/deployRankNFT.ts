c889d9b13ef9d65d9a28c265b4db7b44af1749e2
rexek60 <rexeko601@gmail.com>
2026-06-03 22:14:04 +0300

    Enhance deployRankNFT with owner address and transfeTransfer rank NFT ownership to admin wallet
    Added owner address handling and ownership transfer after deployment.

--- deploy/deployRankNFT.ts
+++ deploy/deployRankNFT.ts
@@ -4,13 +4,21 @@ import { Deployer } from "@matterlabs/hardhat-zksync";
 import { Wallet } from "zksync-ethers";
 
 export default async function deployAbswarRankNFT(hre: HardhatRuntimeEnvironment) {
-      const deployerKey = process.env.DEPLOYER_PRIVATE_KEY || vars.get("DEPLOYER_PRIVATE_KEY");
-      const signerAddress = process.env.RANK_NFT_SIGNER_ADDRESS || vars.get("RANK_NFT_SIGNER_ADDRESS");
-      const wallet = new Wallet(deployerKey);
-      const deployer = new Deployer(hre, wallet);
-      const artifact = await deployer.loadArtifact("AbswarRankNFT");
-      const contract = await deployer.deploy(artifact, [signerAddress]);
+        const deployerKey = process.env.DEPLOYER_PRIVATE_KEY || vars.get("DEPLOYER_PRIVATE_KEY");
+        const signerAddress = process.env.RANK_NFT_SIGNER_ADDRESS || vars.get("RANK_NFT_SIGNER_ADDRESS");
+        const ownerAddress = process.env.RANK_NFT_OWNER_ADDRESS || "0x9C5e9dB5836e9c95be7cBec023D543c36E865B5B";
+        const wallet = new Wallet(deployerKey);
+        const deployer = new Deployer(hre, wallet);
+        const artifact = await deployer.loadArtifact("AbswarRankNFT");
+        const contract = await deployer.deploy(artifact, [signerAddress]);
+        const contractAddress = await contract.getAddress();
 
-  console.log(`AbswarRankNFT deployed to ${await contract.getAddress()}`);
-      console.log(`Rank NFT signer: ${signerAddress}`);
+  if (ownerAddress.toLowerCase() !== wallet.address.toLowerCase()) {
+            const tx = await contract.transferOwnership(ownerAddress);
+            await tx.wait();
+  }
+
+  console.log(`AbswarRankNFT deployed to ${contractAddress}`);
+        console.log(`Rank NFT signer: ${signerAddress}`);
+        console.log(`Rank NFT owner: ${ownerAddress}`);
 }
