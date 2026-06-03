3d61b48085c35a5d02561a1c3db290b85226fa71
rexek60 <rexeko601@gmail.com>
2026-06-03 22:06:19 +0300

    Refactor deployRankNFT to use environment variableSupport env secrets in rank NFT deploy

--- deploy/deployRankNFT.ts
+++ deploy/deployRankNFT.ts
@@ -4,12 +4,13 @@ import { Deployer } from "@matterlabs/hardhat-zksync";
 import { Wallet } from "zksync-ethers";
 
 export default async function deployAbswarRankNFT(hre: HardhatRuntimeEnvironment) {
-  const wallet = new Wallet(vars.get("DEPLOYER_PRIVATE_KEY"));
-  const signerAddress = vars.get("RANK_NFT_SIGNER_ADDRESS");
-  const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact("AbswarRankNFT");
-  const contract = await deployer.deploy(artifact, [signerAddress]);
+    const deployerKey = process.env.DEPLOYER_PRIVATE_KEY || vars.get("DEPLOYER_PRIVATE_KEY");
+    const signerAddress = process.env.RANK_NFT_SIGNER_ADDRESS || vars.get("RANK_NFT_SIGNER_ADDRESS");
+    const wallet = new Wallet(deployerKey);
+    const deployer = new Deployer(hre, wallet);
+    const artifact = await deployer.loadArtifact("AbswarRankNFT");
+    const contract = await deployer.deploy(artifact, [signerAddress]);
 
   console.log(`AbswarRankNFT deployed to ${await contract.getAddress()}`);
-  console.log(`Rank NFT signer: ${signerAddress}`);
+    console.log(`Rank NFT signer: ${signerAddress}`);
 }
